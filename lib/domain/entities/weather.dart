3c8c3428cdd7f5c039a768b27a001f86cff15033
Ali Zare <alizaredev@gmail.com>
2026-06-24 15:10:25 +0330

    add Weather entity: create a new Weather class to represent weather data with relevant properties

---
+++ lib/domain/entities/weather.dart
@@ -0,0 +1,15 @@
+class Weather {
+  final String cityName;
+  final double spped;
+  final double temp;
+  final String main;
+  final int humidity;
+
+  const Weather({
+    required this.cityName,
+    required this.spped,
+    required this.temp,
+    required this.main,
+    required this.humidity,
+  });
+}
